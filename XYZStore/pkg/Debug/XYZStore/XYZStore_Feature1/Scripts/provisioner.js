28b4a0e6c5e76b785f3f968b0b78f1a7f4922e4b
HoussemRomdhani <romdhanihoussem77@gmail.com>
2017-09-19 15:56:01 +0100

    Provisioning with JavaScript: populating products data

--- XYZStore/pkg/Debug/XYZStore/XYZStore_Feature1/Scripts/provisioner.js
+++ XYZStore/pkg/Debug/XYZStore/XYZStore_Feature1/Scripts/provisioner.js
@@ -49,13 +49,84 @@ XYZ.Provisioner = function (appUrl, hostUrl) {
 
         function success() {
             dfd.notify("Products list created");
-            dfd.resolve();
+            getProductsData();
         }
 
         function fail(sender, args) {
             dfd.reject(args);
         }
     }
+
+    function getProductsData() {
+        if (!dfd) return;
+        dfd.notify("Requesting Products data");
+
+        var url = appUrl + "/Content/ProductsData.txt";
+        var call = jQuery.get(url);
+        call.done(function (data, textStatus, jqXHR) {
+            populateProductsList(data);
+        });
+        call.fail(function (jqXHR, textStatus, errorThrown) {
+            dfd.reject(jqXHR);
+        });
+    }
+
+    function populateProductsList(data) {
+        if (!dfd) return;
+        dfd.notify("Populating Products list");
+
+        var context = new SP.ClientContext(appUrl);
+        var web = context.get_web();
+        var list = web.get_lists().getByTitle("Products");
+
+        var products = JSON.parse(data);
+        var currentProduct = -1;
+        updateNextSet();
+
+        function updateNextSet() {
+            var setIndex = 0;
+            while (true) {
+                setIndex += 1;
+                currentProduct += 1;
+                if (setIndex == 25 || currentProduct == products.length) {
+                    context.executeQueryAsync(success, fail);
+                    break;
+                }
+
+                var value = products[currentProduct];
+                var ici = new SP.ListItemCreationInformation();
+                var item = list.addItem(ici);
+                item.set_item("Title", value.Title);
+                item.set_item("QuantityPerUnit", value.QuantityPerUnit);
+                item.set_item("UnitPrice", value.UnitPrice);
+                item.set_item("UnitsInStock", value.UnitsInStock);
+                item.set_item("UnitsOnOrder", value.UnitsOnOrder);
+                item.set_item("ReorderLevel", value.ReorderLevel);
+                item.set_item("Discontinued", value.Discontinued);
+
+                var lfv = new SP.FieldLookupValue();
+                lfv.set_lookupId(value.CategoryId);
+                item.set_item("Category", lfv);
+                item.update();
+            };
+        }
+
+        function success() {
+            dfd.notify(String.format("\t{0} of {1}", currentProduct, products.length));
+            if (currentProduct == products.length) {
+                // updateCurrentVersion();
+                dfd.resolve();
+            } else {
+                updateNextSet();
+            }
+        }
+
+        function fail(sender, args) {
+            dfd.reject(args);
+        }
+
+    }
+
 
     function execute() {
         dfd = new jQuery.Deferred();
