54e506cdc78f436f894ad62e83087c0194499eee
HoussemRomdhani <romdhanihoussem77@gmail.com>
2017-09-19 15:44:25 +0100

    Provisioning with JavaScript: defining a product properties

--- XYZStore/Scripts/provisioner.js
+++ XYZStore/Scripts/provisioner.js
@@ -3,7 +3,28 @@
 XYZ.Provisioner = function (appUrl, hostUrl) {
     var dfd;
 
-    function createProductsList() {
+    function getCategoryListId() {
+        if (!dfd) return;
+        dfd.notify("Getting Category list id");
+
+        var context = new SP.ClientContext(appUrl);
+        var web = context.get_web();
+
+        var list = web.get_lists().getByTitle("Categories");
+
+        context.load(list, "Id");
+        context.executeQueryAsync(success, fail);
+
+        function success() {
+            createProductsList(list.get_id());
+        }
+
+        function fail(sender, args) {
+            dfd.reject(args);
+        }
+    }
+
+    function createProductsList(categoryListId) {
         if (!dfd) return;
         dfd.notify("Creating Products list");
 
@@ -14,6 +35,15 @@ XYZ.Provisioner = function (appUrl, hostUrl) {
         lci.set_title("Products");
         lci.set_templateType(SP.ListTemplateType.genericList);
         var list = web.get_lists().add(lci);
+
+        list.get_fields().addFieldAsXml('<Field DisplayName="Category" Type="Lookup" Required="FALSE" List="{' + categoryListId + '}" Name="Category" ShowField="Title" Version="1" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="QuantityPerUnit" Type="Text" Required="FALSE" Name="QuantityPerUnit" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="UnitPrice" Type="Currency" Required="FALSE" Name="UnitPrice" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="UnitsInStock" Type="Integer" Required="FALSE" Name="UnitsInStock" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="UnitsOnOrder" Type="Integer" Required="FALSE" Name="UnitsOnOrder" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="ReorderLevel" Type="Integer" Required="FALSE" Name="ReorderLevel" />', true, SP.AddFieldOptions.defaultValue);
+        list.get_fields().addFieldAsXml('<Field DisplayName="Discontinued" Type="Boolean" Required="FALSE" Name="Discontinued" />', true, SP.AddFieldOptions.defaultValue);
+
 
         context.executeQueryAsync(success, fail);
 
@@ -30,7 +60,7 @@ XYZ.Provisioner = function (appUrl, hostUrl) {
     function execute() {
         dfd = new jQuery.Deferred();
 
-        createProductsList();
+        getCategoryListId();
 
         return dfd.promise();
     }
