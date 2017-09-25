009bbd06b255d6da5c68a3ef94bb48633230defa
HoussemRomdhani <romdhanihoussem77@gmail.com>
2017-09-25 10:25:32 +0100

    Improving the web part

--- XYZStore/pkg/Debug/XYZStore/XYZStore_Feature1/Scripts/App.js
+++ XYZStore/pkg/Debug/XYZStore/XYZStore_Feature1/Scripts/App.js
@@ -6,17 +6,35 @@
 
     jQuery(document).ready(function () {
         jQuery("#addWebPartFilesButton").click(addWebPartFiles);
+        jQuery("#removeWebPartFilesButton").click(removeWebPartFiles);
     });
 
     function addWebPartFiles() {
         UpdateFormDigest(_spPageContextInfo.webServerRelativeUrl, _spFormDigestRefreshInterval);
 
+        var webPartFileName = "databinding.dwp";
         var call1 = copyFile("WebPartContent", "Site Assets", "databinding.txt");
-        var call2 = copyFile("WebPartContent", "Web Part Gallery", "databinding.dwp");
+        var call2 = getFile("WebPartContent", webPartFileName)
+            .then(updateContentLink)
+            .then(function (fileContents) { return uploadFile(fileContents, "Web Part Gallery", webPartFileName) })
+            .then(updateWebPartGroup)
         var calls = jQuery.when(call1, call2);
         calls.done(function (response1, response2) {
             var message = jQuery("#message");
             message.text("Web Part files copied");
+        });
+        calls.fail(failHandler);
+    }
+
+    function removeWebPartFiles() {
+        UpdateFormDigest(_spPageContextInfo.webServerRelativeUrl, _spFormDigestRefreshInterval);
+
+        var call1 = deleteFile("Site Assets", "databinding.txt");
+        var call2 = deleteFile("Web Part Gallery", "databinding.dwp");
+        var calls = jQuery.when(call1, call2);
+        calls.done(function (response1, response2) {
+            var message = jQuery("#message");
+            message.text("Web Part files removed");
         });
         calls.fail(failHandler);
     }
@@ -37,7 +55,7 @@
 
     function uploadFile(fileContents, targetLibrary, fileName) {
         var url = String.format("{0}/_api/SP.AppContextSite(@target)" +
-            "/Web/Lists/getByTitle('{1}')/RootFolder/Files/Add(url='{2}',overwrite=true)?@target='{3}'",
+            "/Site/RootWeb/Lists/getByTitle('{1}')/RootFolder/Files/Add(url='{2}',overwrite=true)?@target='{3}'",
             appUrl, targetLibrary, fileName, hostUrl);
 
         var call = jQuery.ajax({
@@ -58,6 +76,61 @@
     function copyFile(sourceFolder, targetLibrary, fileName) {
         var call = getFile(sourceFolder, fileName)
             .then(function (fileContents) { return uploadFile(fileContents, targetLibrary, fileName) });
+
+        return call;
+    }
+
+    function updateWebPartGroup(data) {
+        var file = data.d;
+        var url = String.format("{0}/_api/SP.AppContextSite(@target)" +
+            "/Site/RootWeb/GetFileByServerRelativeUrl('{1}')/ListItemAllFields?" +
+            "@target='{2}'",
+            appUrl, file.ServerRelativeUrl, hostUrl);
+
+        var call = jQuery.ajax({
+            url: url,
+            type: "POST",
+            data: JSON.stringify({
+                "__metadata": { type: "SP.Data.OData__x005f_catalogs_x002f_wpItem" },
+                Group: "App Script Part"
+            }),
+            headers: {
+                Accept: "application/json;odata=verbose",
+                "Content-Type": "application/json;odata=verbose",
+                "X-RequestDigest": jQuery("#__REQUESTDIGEST").val(),
+                "IF-MATCH": "*",
+                "X-Http-Method": "PATCH"
+            }
+        });
+
+        return call;
+    }
+
+    function updateContentLink(fileContents) {
+        var def = new jQuery.Deferred();
+
+        var fileUrl = hostUrl + "/SiteAssets/databinding.txt";
+        fileContents = fileContents.replace("{ContentLink}", fileUrl);
+        def.resolve(fileContents);
+
+        return def.promise();
+    }
+
+    function deleteFile(targetLibrary, fileName) {
+        var url = String.format("{0}/_api/SP.AppContextSite(@target)" +
+            "/Site/RootWeb/Lists/getByTitle('{1}')/RootFolder/Files('{2}')?@target='{3}'",
+            appUrl, targetLibrary, fileName, hostUrl);
+
+        var call = jQuery.ajax({
+            url: url,
+            type: "POST",
+            headers: {
+                Accept: "application/json;odata=verbose",
+                "X-RequestDigest": jQuery("#__REQUESTDIGEST").val(),
+                "IF-MATCH": "*",
+                "X-Http-Method": "DELETE"
+            }
+        });
 
         return call;
     }
